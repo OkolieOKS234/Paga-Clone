22fa4737079fe4a0f9ac7c18316036fb67fe507c
OkolieOKS234 <dokolie040@gmail.com>
2023-05-04 16:25:22 +0100

    Made the hero section better

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,19 @@ import GadgetsImage from "../assets/laptopandPhone.png"
 
 const Hero = () => {
   return (
-    <section className='flex mt-24 ml-12'>
-      <div className="max-w-lg">
-      <p>Your cash, anywhere, anytime</p> 
-      <h1 className='text-3xl font-semibold'>The Mobile payment operator for you and your business</h1>
-      <p>We are building an ecosystem to enable people to digitally send and receive money, creating simple financial access for everyone.</p>
+    <section className='flex mt-24 ml-24 items-center'>
+      <div className="heading max-w-lg">
+      <p className='text-orange-500'>Your cash, anywhere, anytime</p> 
+      <h1 className='header_text text-3xl font-semibold'>The Mobile payment operator for you and your business</h1>
+      <p className='text-xs'>We are building an ecosystem to enable people to digitally send and receive money, creating simple financial access for everyone.</p>
+     <div className='flex items-center'>
+     <button className='bg-orange-500 py-2 px-3 mt-6'>Create an account</button>
+     <p className='self-center justify-self-center py-2 px-3 mt-6 text-orange-500 '>Learn More</p>
+     </div>
+     
       </div>
-      <div>
+     
+      <div className=" max-w-lg">
         <img src={GadgetsImage} alt="" />
       </div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,8 +19,8 @@ let [open, setOpen] = useState(false);
 
   return (
     <div className=' navbar w-full fixed top-0 left-0 '>
-    <div className='  bg-white md:flex items-center py-4 md:px-2 px-7 gap-7 '>
-    <div className='font-bold bg-white  cursor-pointer flex items-center font-[Poppins] 
+    <div className='md:flex items-center py-4 md:px-2 px-7 gap-7 ml-12  '>
+    <div className='font-bold   cursor-pointer flex items-center font-[Poppins] 
     text-gray-800'>
       <div className=' mr-1 p-0 m-0 '>
       <img src={LogoImage} alt="Maro_logo" className='ml-7 '/>
@@ -31,11 +31,11 @@ let [open, setOpen] = useState(false);
     <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden text-gray-900'>
     <ion-icon name={open ? 'close':'menu'}></ion-icon>
     </div>
-    <ul className={` bg-white md:flex md:items-center md:pb-0 pb-2  absolute md:static  z-[2]  w-full  md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+    <ul className={`  md:flex md:items-center md:pb-0 pb-2  absolute md:static  z-[2]  w-full  md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
         {
           Links.map((link)=>(
             
-            <ul key={link.name} to={link.link} className='md:ml-6 text-sm md:my-0 my-7 text-orange-500 ' smooth >
+            <ul key={link.name} to={link.link} className='md:ml-6 text-sm md:my-0 my-7  text-orange-500 ' smooth >
               <li className='text-orange-500 hover:text-gray-400 duration-500 '>{link.name}</li>
            
             </ul>
