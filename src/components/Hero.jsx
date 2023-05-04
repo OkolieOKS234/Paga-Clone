4df6a2cc6a018f5c2b5a2d71e498c6914dd5e0de
OkolieOKS234 <dokolie040@gmail.com>
2023-05-04 16:02:33 +0100

    added the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import GadgetsImage from "../assets/laptopandPhone.png"
 
 const Hero = () => {
   return (
-    <section>
-     <p>Your cash, anywhere, anytime</p> 
-     <h1>The Mobile payment operator for you and your business</h1>
-     <p>We are building an ecosystem to enable people to digitally send and receive money, creating simple financial access for everyone.</p>
+    <section className='flex mt-24 ml-12'>
+      <div className="max-w-lg">
+      <p>Your cash, anywhere, anytime</p> 
+      <h1 className='text-3xl font-semibold'>The Mobile payment operator for you and your business</h1>
+      <p>We are building an ecosystem to enable people to digitally send and receive money, creating simple financial access for everyone.</p>
+      </div>
+      <div>
+        <img src={GadgetsImage} alt="" />
+      </div>
     </section>
   )
 }
