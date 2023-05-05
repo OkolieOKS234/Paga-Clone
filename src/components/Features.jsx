0b4abdb8d88d72e055f7dff591b603652b2d6058
OkolieOKS234 <dokolie040@gmail.com>
2023-05-05 12:01:48 +0100

    added new features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -10,7 +10,7 @@ import TopupIcon from "../assets/icons/Airtime_Data.png"
 const Features = () => {
   return (
     <section className='bg-white '>
-      <h2 className='ml-24'>Join over <span className='text-orange-500'>19million</span>people who use paga to</h2>
+      <h2 className='ml-24 text-4xl my-6 w-96'>Join over <span className='text-orange-500'>19million</span> people who use paga to</h2>
     {/* Features Boxes */}
     <div className='flex gap-4 flex-wrap items-center justify-center'>
 
@@ -26,17 +26,29 @@ const Features = () => {
 <p className='text-xs'>Receive instant and secure money transfers from abroad to any Nigerian mobile phone number or bank account , using paga.</p>
 <p className='text-orange-500'>Read More</p>
 </div>
-<div className='box'>
+<div className='box max-w-sm px-12 py-6'>
 <img src={ReceiveMoneyIcon} alt="" />
+<p className='text-orange-500'>Receive Money</p>
+<p className='text-xs'> Receive money instantly from anyone with a phone number, email address or bank account.</p>
+<p className='text-orange-500'>Read More</p>
 </div>
-<div className='box'>
+<div className='box max-w-sm px-12 py-6'>
   <img src={RequestMoneyIcon} alt="" />
+  <p className='text-orange-500'>Request Money</p>
+  <p className='text-xs'>Request money instantly from anyone with a phone number or email address.</p>
+  <p className='text-orange-500'>Read More</p>
 </div>
-<div className='box'>
+<div className='box max-w-sm px-12 py-6'>
   <img src={PayBillsIcon} alt="" />
+  <p className='text-orange-500'>Pay Bills</p>
+  <p className='text-xs'>Renew your Pay TV, Internet, Electricity and so much more with Paga’s smooth bill payment services.</p>
+  <p className='text-orange-500'>Read More</p>
 </div>
-<div className='box'>
+<div className='box max-w-sm px-12 py-6'>
   <img src={TopupIcon} alt="" />
+  <p className='text-orange-500'>Top-up Airtime & Data</p>
+  <p className='text-xs'>Top up your data and airtime instantly using Paga.</p>
+  <p className='text-orange-500'>Read More</p>
 </div>
 
     </div>
